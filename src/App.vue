2eda3ce83464fbbdc87f1472af4b5d23b6eded0c
Kosse <sd103e@mail.ru>
2024-10-17 12:28:29 +0300

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,65 @@
-<script setup>
-import { onMounted } from 'vue'
-import { initFlowbite } from 'flowbite'
+<script setup lang="ts">
+import {onMounted, ref, watch} from 'vue'
+import {initFlowbite} from 'flowbite'
 import axios from "axios";
+import Tree from "@/components/Tree.vue"
+import Loader from "@/components/Loader.vue";
+
+const nodes = ref<Object>(null)
+const allowEmpty = ref<Boolean>(false)
+const totalCount = ref<Number>(0)
+
+function fetchNodes() {
+  axios.get(`https://www.klerk.ru/yindex.php/v3/event/rubrics?allowEmpty=${allowEmpty.value ? 1 : 0}`)
+      .then(({data}) => nodes.value = data)
+}
+
+watch(allowEmpty, () => {
+  nodes.value = null
+  totalCount.value = 0
+
+  fetchNodes()
+})
 
 onMounted(() => {
-  initFlowbite();
-
-  axios.get('https://www.klerk.ru/yindex.php/v3/event/rubrics')
-      .then( ({data}) => console.log(data))
+  initFlowbite()
+  fetchNodes()
 })
+
+function summing(count) {
+  totalCount.value += count
+}
 </script>
 
 <template>
-  <div>
-    <div class="flex justify-center p-4">
-      <button id="button" data-modal-toggle="modal" data-modal-target="modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Show modal</button>
-    </div>
 
-    <div id="modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-      <div class="relative w-full max-w-2xl max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <!-- Modal header -->
-          <div class="flex items-start justify-between p-5 border-b rounded-t dark:border-gray-600">
-            <h3 class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">
-              Terms of Service
-            </h3>
-            <button id="closeButton" data-modal-hide="modal" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
-              <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </button>
-          </div>
-          <!-- Modal body -->
-          <div class="p-6 space-y-6">
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-            </p>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-            </p>
-          </div>
-          <!-- Modal footer -->
-          <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-            <button type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Decline</button>
-          </div>
-        </div>
-      </div>
-    </div>
+<div class="flex items-center justify-between">
+  <div class="font-medium text-gray-300">
+    Сумма выделенных элементов:
+    <span class="font-bold text-lg">{{ totalCount }}</span>
   </div>
+  <div class="checkbox-container flex items-center my-2 gap-2">
+    <input id="allow-empty-checkbox" type="checkbox" v-model="allowEmpty"
+           class="text-green-700 rounded focus:ring-green-600 ring-offset-neutral-800 focus:ring-2 bg-neutral-800 border-neutral-600">
+    <label for="allow-empty-checkbox" class="font-medium text-gray-300">Отображать пустые рубрики</label>
+  </div>
+</div>
+
+  <ul class="border border-green-400">
+
+    <Loader v-if="!nodes" class="flex justify-center m-8"/>
+
+    <Tree v-for="node in nodes" :key="node.id"
+          :node="node"
+          @select="summing"
+    />
+  </ul>
 </template>
+
+<style scoped lang="scss">
+.checkbox-container {
+  input, label {
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/components/Tree.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+
+import {computed, ref, watch} from "vue";
+
+const props = defineProps<{
+  node: Object,
+  isSelected?: boolean
+}>()
+const emit = defineEmits(['select'])
+
+const active = ref<Boolean>(false)
+const hasChildren = ref<Boolean>(!!props.node.children?.length)
+const selected = ref<Boolean>(props.isSelected)
+
+function toggle() {
+  if (hasChildren.value)
+    active.value = !active.value
+}
+
+watch(() => props.isSelected, (value) => {
+  selected.value = value
+})
+
+watch(selected, () => {
+  let sign = selected.value ? 1 : -1
+  bubble(props.node.count * sign)
+})
+
+function bubble(v) {
+  emit('select', v)
+}
+
+const countSum = computed(() => {
+  function r(node, sum) {
+    sum += node.count
+
+    if (node.children) {
+      for (let child of node.children)
+        sum = r(child, sum)
+
+      return sum
+    }
+
+    return sum
+  }
+
+  return r(props.node, 0)
+})
+</script>
+
+<template>
+  <li :class="['p-1', 'border-l',
+              {'border-b': hasChildren}, 'border-green-400']"
+  >
+    <div :class="['px-3', 'py-3', 'flex justify-between items-center gap-x-3', 'cursor-pointer', 'transition', 'duration-300',
+                 'hover:bg-green-800', {'bg-green-900': active}, {'hover:bg-neutral-800': !hasChildren},
+                 {'border-l-4': active}, 'border-green-500']"
+         @click.stop="toggle"
+    >
+      <div>
+        <a :href="`https://www.klerk.ru${node.url}`" target="_blank"
+           class="transition duration-300 hover:text-gray-300 hover:underline hover:font-bold"
+           @click.stop
+        >
+          <span class="me-1">{{ node.title }}</span>
+
+          (
+          <span>Свой: {{ node.count }}</span> |
+          <span>Сумма: {{ countSum }}</span>
+          )
+        </a>
+      </div>
+
+      <input id="select-checkbox" type="checkbox" v-model="selected" @click.stop
+             class="cursor-pointer text-green-700 rounded focus:ring-green-600 ring-offset-neutral-800 focus:ring-2 bg-neutral-800 border-neutral-600">
+    </div>
+
+    <Tree v-show="active" v-for="child in node.children" :key="child.id"
+          :node="child"
+          :isSelected="selected"
+          @select="bubble"
+    />
+  </li>
+</template>
